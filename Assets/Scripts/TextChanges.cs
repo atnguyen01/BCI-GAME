00eff75071bdfe6688f6014faf55f1f640892f8a
Andrew <atnguyen@wpi.edu>
2023-11-17 10:04:05 -0500

    button text changes now too

--- Assets/Scripts/TextChanges.cs
+++ Assets/Scripts/TextChanges.cs
@@ -11,6 +11,9 @@ public class TextChanges : MonoBehaviour
         public GameObject TSImanager;
         private TSINetworkInterface tSINetworkInterface;
         public TMP_Text jim;
+        public TMP_Text buttonlabel1;
+        public TMP_Text buttonlabel2;
+        public TMP_Text buttonlabel3;
         public int baker = 0;
         public int carpenter = 0;
         public int potter = 0;
@@ -28,6 +31,7 @@ public class TextChanges : MonoBehaviour
     public void UpdateField(Microsoft.MixedReality.Toolkit.UI.Interactable index)
     {
         //button_name = index.gameObject.name;
+        //set button number for clickresponse to use as switch case 
            if(index.gameObject.name.Equals("Pressable Button (1)")){
                 button_num = 1;
             } 
@@ -53,43 +57,68 @@ public class TextChanges : MonoBehaviour
            // i.OnClick.AddListener(() => Debug.Log(Time.time + ": " + i.gameObject.name + " was clicked"));
             //i.OnClick.AddListener(() =>button_name = i.gameObject.name);
             i.OnClick.AddListener(() => UpdateField(i));
-            //set button number for clickresponse to use as switch case 
+            i.OnClick.AddListener(() => response_counter += 1);
+            i.OnClick.AddListener(() => playTurn(response_counter, button_num));
+            }
             /*
-            if(i.gameObject.name.Equals("Pressable Button (1)")){
-                button_num = 1;
-            } 
-            if(i.gameObject.name.Equals("Pressable Button (2)")){
-                button_num = 2;
-            } 
-            if(i.gameObject.name.Equals("Pressable Button (3)")){
-                button_num = 3;
-            }
-            */
-            }
         Debug.Log(button_name);
         response_counter += 1;
         Debug.Log(response_counter);
         Debug.Log("Button num: " + button_num);
         playTurn(response_counter, button_num);
+        */
+    }
+
+    public void setButtonText(int turn_num){
+        switch(turn_num){
+            case 1:
+            buttonlabel1.text = "Did Andrew do it?";
+            buttonlabel2.text = "Who's James?";
+            buttonlabel3.text = "Is Jagger sus?!?!?!?";
+            break;
+            case 2:
+            buttonlabel1.text = "Cool";
+            buttonlabel2.text = "Nice!";
+            buttonlabel3.text = "Called it";
+            break;
+        }
     }
 
     public void playTurn(int turn_num, int button_number){
-       // switch(turn_num){
-          //  case 1:
+       switch(turn_num){
+          case 1:
             switch(button_num){
                 case 1:
                 jim.text = "FNAF is the best movie ever";
+                setButtonText(turn_num);
                 break;
                 case 2:
                 jim.text = "Skyrim is the best game ever";
+                setButtonText(turn_num);
                 break;    
                 case 3:
                 jim.text = "Jagger is always correct";
+                setButtonText(turn_num);
                 break;
-          //  }
-           // break;
-     //   }
-    }
+               }
+           break;
+            case 2:
+                switch(button_num){
+                    case 1:
+                    jim.text = "Andrew is innocent";
+                    setButtonText(turn_num);
+                    break;
+                    case 2:
+                    jim.text = "james is a baker. He bakes many good things like breads,cakes, and pastries. I especially like bagels with cream cheese it's just sooooooooooooooooo good";
+                    setButtonText(turn_num);
+                    break;    
+                    case 3:
+                    jim.text = "Jagger did it";
+                    setButtonText(turn_num);
+                    break;
+                    }
+           break;
+}
 }
 }
 }
