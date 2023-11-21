23dec4abd0965f8324a8db06533308f5cbaa9ac6
Andrew <atnguyen@wpi.edu>
2023-11-21 10:06:32 -0500

    change ui colors, fixed bugs with response counter not adding correctly

--- Assets/Scripts/TextChanges.cs
+++ Assets/Scripts/TextChanges.cs
@@ -17,7 +17,7 @@ public class TextChanges : MonoBehaviour
         public int baker = 0;
         public int carpenter = 0;
         public int potter = 0;
-        public int response_counter = 0;
+        public int response_counter = 0; //-2 to deal with the double click bug at beginning
         public string clickedButton = "";
         public int button_num = 0;
         public string button_name = null;
@@ -27,6 +27,13 @@ public class TextChanges : MonoBehaviour
     void Start()
     {
         tSINetworkInterface = TSImanager.GetComponent<TSINetworkInterface>();
+                var allInteractables = GameObject.FindObjectsOfType<Interactable>();
+            foreach (var i in allInteractables)
+            {
+           // i.OnClick.AddListener(() => Debug.Log(Time.time + ": " + i.gameObject.name + " was clicked"));
+            //i.OnClick.AddListener(() =>button_name = i.gameObject.name);
+            i.OnClick.AddListener(() => UpdateField(i));
+            }
     }
 
     public void UpdateField(Microsoft.MixedReality.Toolkit.UI.Interactable index)
@@ -52,15 +59,17 @@ public class TextChanges : MonoBehaviour
 
     public void clickresponse(){
  //       button_name = GetComponent<Interactable>().name;
+ /*
         var allInteractables = GameObject.FindObjectsOfType<Interactable>();
             foreach (var i in allInteractables)
             {
            // i.OnClick.AddListener(() => Debug.Log(Time.time + ": " + i.gameObject.name + " was clicked"));
             //i.OnClick.AddListener(() =>button_name = i.gameObject.name);
             i.OnClick.AddListener(() => UpdateField(i));
-            i.OnClick.AddListener(() => response_counter += 1);
-            i.OnClick.AddListener(() => playTurn(response_counter, button_num));
+            i.OnClick.AddListener(() => playTurn(response_counter));
+            //i.OnClick.AddListener(() => response_counter += 1);
             }
+            */
             /*
         Debug.Log(button_name);
         response_counter += 1;
@@ -71,6 +80,7 @@ public class TextChanges : MonoBehaviour
     }
 
     public void setButtonText(int turn_num){
+        Debug.Log("button: "+ button_num);
         switch(turn_num){
             case 1:
             buttonlabel1.text = "Did Andrew do it?";
@@ -82,29 +92,44 @@ public class TextChanges : MonoBehaviour
             buttonlabel2.text = "Nice!";
             buttonlabel3.text = "Called it";
             break;
+            case 3:
+            buttonlabel1.text = "wha";
+            buttonlabel2.text = "cool!";
+            buttonlabel3.text = "knew it was coming";
+            break;
+            case 4:
+            buttonlabel1.text = "Cool";
+            buttonlabel2.text = "Nice!";
+            buttonlabel3.text = "Called it";
+            break;
+            case 5:
+            buttonlabel1.text = "wha";
+            buttonlabel2.text = "cool!";
+            buttonlabel3.text = "knew it was coming";
+            break;
         }
     }
 
-    public void playTurn(int turn_num, int button_number){
-       switch(turn_num){
+    public void playTurn(){
+       switch(response_counter){
           case 1:
             switch(button_num){
                 case 1:
                 if(isFocused){
                 jim.text = "FNAF is the best movie ever";
-                setButtonText(turn_num);
+                setButtonText(response_counter);
                 }else {
                 jim.text = "FNAF is the worst movie ever";
-                setButtonText(turn_num);
+                setButtonText(response_counter);
                 }
                 break;
                 case 2:
                 jim.text = "Skyrim is the best game ever";
-                setButtonText(turn_num);
+                setButtonText(response_counter);
                 break;    
                 case 3:
                 jim.text = "Jagger is always correct";
-                setButtonText(turn_num);
+                setButtonText(response_counter);
                 break;
                }
            break;
@@ -112,19 +137,69 @@ public class TextChanges : MonoBehaviour
                 switch(button_num){
                     case 1:
                     jim.text = "Andrew is innocent";
-                    setButtonText(turn_num);
+                    setButtonText(response_counter);
                     break;
                     case 2:
                     jim.text = "james is a baker. He bakes many good things like breads,cakes, and pastries. I especially like bagels with cream cheese it's just sooooooooooooooooo good";
-                    setButtonText(turn_num);
+                    setButtonText(response_counter);
                     break;    
                     case 3:
                     jim.text = "Jagger did it";
-                    setButtonText(turn_num);
+                    setButtonText(response_counter);
+                    break;
+                    }
+           break;
+            case 3:
+                switch(button_num){
+                    case 1:
+                    jim.text = "Andrew is a wizard";
+                    setButtonText(response_counter);
+                    break;
+                    case 2:
+                    jim.text = "james is a baker.";
+                    setButtonText(response_counter);
+                    break;    
+                    case 3:
+                    jim.text = "Jagger sus";
+                    setButtonText(response_counter);
+                    break;
+                    }
+           break;
+            case 4:
+                switch(button_num){
+                    case 1:
+                    jim.text = "Andrew is a potter";
+                    setButtonText(response_counter);
+                    break;
+                    case 2:
+                    jim.text = "james is just here.";
+                    setButtonText(response_counter);
+                    break;    
+                    case 3:
+                    jim.text = "Jagger is a gamer";
+                    setButtonText(response_counter);
+                    break;
+                    }
+           break;
+            case 5:
+                switch(button_num){
+                    case 1:
+                    jim.text = "Andrew is cool";
+                    setButtonText(response_counter);
+                    break;
+                    case 2:
+                    jim.text = "james is good with bread knives.";
+                    setButtonText(response_counter);
+                    break;    
+                    case 3:
+                    jim.text = "Jagger hates all of us";
+                    setButtonText(response_counter);
                     break;
                     }
            break;
 }
+response_counter += 1;
+Debug.Log(response_counter);
 }
 }
 }
